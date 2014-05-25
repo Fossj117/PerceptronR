d85e142ec815d59fef088e550beeb7b11acd0b20
Fossj117 <fossj117@gmail.com>
2014-05-25 14:47:43 -0400

    Updated perceptron code. Now working

--- Perceptron2.R
+++ Perceptron2.R
@@ -1,6 +1,8 @@
 ###Perceptron playing
 
 add_boundary <- function(w, color='red'){
+  w <- as.numeric(w)  
+  
   b = w[1]
   w1 = w[2]
   w2 = w[3]
@@ -13,9 +15,12 @@ add_boundary <- function(w, color='red'){
 activate <- function(df,w){ 
   #df should be of shape m x n
   #w should be of shape 1 x n
-  activation <- apply(df, 1, function(x)sum(x*w))
+  activation <- apply(df, 1, function(x) sum(x*w))
   return(ifelse(activation > 0, 1, -1))
 }
+
+
+#### GENERATE THE DATA ####
 
 #randomly choose the TRUE parameters
 w_true <- runif(3, -5, 5)
@@ -32,36 +37,35 @@ f_class <- activate(df, w_true) #get true classifications using w_true
 plot(x1,x2, col=factor(f_class))
 add_boundary(w_true, color='blue')
 
-#### Learning ###
+#### LEARNING ####
 
 #randomly initialize estimate parameters
 w_guess <- runif(3, -.5, .5)
 add_boundary(w_guess, color='green') #plot current hypothesis
-
-g_class <- activate(df, w_guess)
+g_class <- activate(df, w_guess) #stores classifications with current w_guess
 
 ## Start PLA ###
 
-max_iter <- 100 #maximum number of iterations
-n_misses <- rep(NA, 100) #because R
+#max_iter <- 100 #maximum number of iterations
+#n_misses <- rep(NA, 100) #because R
 
 while(!all(g_class==f_class)){ #while there are mis-classifications
   
   #randomly choose a mis-classified example
   mis_classifs <- df[g_class != f_class,]  
-  rand_choice <- sample(1:nrow(mis_classifs), 1) #index
+  
+  rand_choice <- if(nrow(mis_classifs)==1){
+    as.numeric(rownames(mis_classifs))
+  }else{
+    sample(as.numeric(rownames(mis_classifs)),1) #choose one of the misclassed rows
+  }
   
   #update the weights
-  w_guess <- w_guess + f_class[rand_choice]*as.vector(mis_classifs[rand_choice,])
+  w_guess <- w_guess + f_class[rand_choice]*as.vector(mis_classifs[rownames(mis_classifs)==rand_choice,])
   
-  g_class <- activate(d, w_guess)
+  g_class <- activate(df, w_guess)
   
-  gw1 <- gw1 + alpha*rand_choice$f_class*rand_choice$x1
-  gw2 <- gw2 + alpha*rand_choice$f_class*rand_choice$x2
-  gb <- gb + alpha*rand_choice$f_class*1
-  
-  add_boundary(gw1, gw2, gb, color="red")
-  df$g_class <- fun(df$x1, df$x2, gw1, gw2, gb)
+  add_boundary(w_guess, color='pink')
   
   readline("Press <return> to continue") 
 }
